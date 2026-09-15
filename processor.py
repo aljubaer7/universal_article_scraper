b190f0803ce70fdc3a1fa17945b8a82ffabf7390
Jubaer <a.aljubaer7@gmail.com>
2026-09-15 06:28:10 +0600

    replace dataframe with dicts of list in for loop

--- processor.py
+++ processor.py
@@ -3,10 +3,7 @@ import uuid
 import json
 import statistics
 import unicodedata
-import numpy as np
-import pandas as pd
 from datetime import datetime
-from collections import Counter
 
 # pronouns & prepositions
 with open(r'data\pnoun_prep.txt', 'r') as f:
@@ -67,38 +64,6 @@ class TextValidator:
         else:
             return False
             
-    def word_dict(self, text):
-        words = re.sub(r"[,.?;:'!()-]", '', text).split()
-        return Counter(words)
-    def calculate_similarity(self, x, y):
-        dict_x = self.word_dict(x)
-        dict_y = self.word_dict(y)
-        comm_words = set(dict_x.keys()) | set(dict_y.keys())
-        matrix_x = np.array([dict_x.get(word, 0) for word in comm_words])
-        matrix_y = np.array([dict_y.get(word, 0) for word in comm_words])
-        dot_product = np.dot(matrix_x, matrix_y)
-        norm_x = np.linalg.norm(matrix_x)
-        norm_y = np.linalg.norm(matrix_y)
-        norm_xy = norm_x * norm_y
-        if norm_xy > 0:
-            return dot_product / norm_xy
-        else:
-            return 0        
-    def exclude_simi_lines(self, text_lines: list):
-        '''Exclude similar lines (internal) from a given list of text-lines'''
-        exc_idx = set()
-        for l in range(0, len(text_lines) - 1):
-            for r in range(l + 1, len(text_lines)):
-                if l != r:
-                    similarity = self.calculate_similarity(text_lines[l], text_lines[r])
-                    if len(text_lines[l]) > 50 and similarity > self.parameters['max_int_txt_simi']:
-                        exc_idx.add(max(l, r))
-                    elif len(text_lines[l]) < 50 and similarity > self.parameters['max_int_txt_simi'] - 0.05:
-                        exc_idx.add(l)
-                        exc_idx.add(r)
-        uniq_text_lines = [text_lines[i] for i in range(len(text_lines)) if i not in exc_idx]
-        return uniq_text_lines
-
     def _character_maping(self, text_lines) -> list:
         result = []
         for line in text_lines:
@@ -308,11 +273,11 @@ class SearchByArticle:
             if validator.is_valid(text_lines):
                 sentence_score = validator.sentence_score(text_lines)
                 if sentence_score > self.parameters['min_sent_score']:
+
                     metadata = {'tag': self.tag, 'attribute': 'n/a'}
                     metadata.update(validator.create_metadata(text_lines))
                     metadata.update({'sentence_score': sentence_score})
-                    df = pd.DataFrame([metadata])
-                    return text_lines, df
+                    return text_lines, metadata
         return None, None
     
 
@@ -324,7 +289,7 @@ class SearchByAttributes:
         self.parameters = parameters
 
     def loop_attributes(self):
-        df = pd.DataFrame()
+        metadata = []
 
         parser = SoupToTextlines(self.soup, self.parameters)
         attributes: list = parser.get_attributes(self.tags)
@@ -338,18 +303,19 @@ class SearchByAttributes:
                     # sentence scoring
                     sentence_score = validator.sentence_score(text_lines)
                     if sentence_score > self.parameters['min_sent_score']:
-                        metadata = {'tag': tag, 'attribute': item}
-                        metadata.update(validator.create_metadata(text_lines))
-                        metadata.update({'sentence_score': sentence_score})
 
-                        dfr = pd.DataFrame([metadata])
-                        df = pd.concat([df, dfr], ignore_index=True)
-        if len(df) > 0:
-            df = df.sort_values(by='sentence_score', ascending=False, ignore_index=True)
-            tag = df.tag.iloc[0]
-            attribute = df.attribute.iloc[0]
+                        meta = {'tag': tag, 'attribute': item}
+                        meta.update(validator.create_metadata(text_lines))
+                        meta.update({'sentence_score': sentence_score})
+                        metadata.append(meta)
+
+        if len(metadata) > 0:
+            metadata = sorted(metadata, key=lambda on: on['sentence_score'], reverse=True)
+
+            tag = metadata[0].get('tag')
+            attribute = metadata[0].get('attribute')
             text_lines = parser.get_textLines(tag, attribute)
-            return text_lines, df
+            return text_lines, metadata
         return None, None
 
 
@@ -378,27 +344,3 @@ def save_text(url, title, text_lines, df, txt_file, sql=False):
         # send to sql
         sql.to_sql(table_name, conn, if_exists='append', index=False)
         conn.commit()
-
-# prevent same text processing multiple times by calculating similarity
-def vector_similarity(self, text_lines, text_vector):
-    all_text = self.soup.get_text(strip=True).lower()
-    all_words = list(dict.fromkeys(re.sub(r"[,.?;:'!()-]", '', all_text).split()))
-
-    text = ' '.join(text_lines).lower()
-    words = re.sub(r"[,.?;:'!()-]", '', text).split()
-    words_dict = Counter(words)
-    x = [words_dict.get(word, 0) for word in all_words] # current text matrix.
-
-    similarities = []
-    for i in range(len(text_vector)):
-        y = text_vector[i]
-        dot_product = np.dot(x, y)
-        norm_x = np.linalg.norm(x)
-        norm_y = np.linalg.norm(y)
-        norm_xy = norm_x * norm_y
-        if norm_xy > 0:
-            similarity = dot_product / norm_xy
-            similarities.append(similarity)
-    text_vector.append(x)
-    max_simi = max(similarities) if similarities else 0
-    return max_simi
